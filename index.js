f7cdd4227d590162c980e525b200edf656db41b1
Artem <sirartemis91@gmail.com>
2024-08-30 00:27:02 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 // Generar un identificador único para esta instancia
 const instanceId = Math.random().toString(36).substring(7);
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send(`Hello from instance ${instanceId}`);
 });
 
